c99b0aded3c638bebfa9a6d4f1534c4f3ad66983
Shokhrukhbek Yuldoshev <shokhrukhbekdev@gmail.com>
2023-12-05 06:33:11 +0300

    advanced arrays added

---
+++ 2-advanced/1-arrays/4-two-pointers.py
@@ -0,0 +1,31 @@
+# https://leetcode.com/problems/valid-palindrome/
+# https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+# https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+# https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
+# https://leetcode.com/problems/container-with-most-water/
+# https://leetcode.com/problems/trapping-rain-water/
+
+# Given a string of characters, return true if it's a palindrome,
+# return false otherwise: O(n)
+def isPalindrome(word):
+    L, R = 0, len(word) - 1
+    while L < R:
+        if word[L] != word[R]:
+            return False
+        L += 1
+        R -= 1
+    return True
+
+# Given a sorted array of integers, return the indices
+# of two elements (in different positions) that sum up to
+# the target value. Assume there is exactly one solution.
+# O(n)
+def targetSum(nums, target):
+    L, R = 0, len(nums) - 1
+    while L < R:
+        if nums[L] + nums[R] > target:
+            R -= 1
+        elif nums[L] + nums[R] < target:
+            L += 1
+        else:
+            return [L, R]
